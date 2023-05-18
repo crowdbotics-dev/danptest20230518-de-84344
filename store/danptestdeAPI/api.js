7d4f7b70c411d545e85728c9ee77303567583bfd
daniel palacios <daniel.palacios@crowdbotics.com>
2023-05-18 10:08:21 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/danptestdeAPI/api.js
+++ store/danptestdeAPI/api.js
@@ -1,55 +1,72 @@
-import axios from "axios"
+import axios from "axios";
 const danptestdeAPI = axios.create({
   baseURL: "https://danptest20230518-de-84344.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_v1_login_create(payload) {
-  return danptestdeAPI.post(`/api/v1/login/`, payload.data)
+  return danptestdeAPI.post(`/api/v1/login/`, payload.data);
 }
+
 function api_v1_signup_create(payload) {
-  return danptestdeAPI.post(`/api/v1/signup/`, payload.data)
+  return danptestdeAPI.post(`/api/v1/signup/`, payload.data);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return danptestdeAPI.get(`/rest-auth/user/`)
+  return danptestdeAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return danptestdeAPI.put(`/rest-auth/user/`, payload.data)
+  return danptestdeAPI.put(`/rest-auth/user/`, payload.data);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return danptestdeAPI.patch(`/rest-auth/user/`, payload.data)
+  return danptestdeAPI.patch(`/rest-auth/user/`, payload.data);
 }
+
 function api_docs_schema_retrieve(payload) {
   return danptestdeAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function rest_auth_login_create(payload) {
-  return danptestdeAPI.post(`/rest-auth/login/`, payload.data)
+  return danptestdeAPI.post(`/rest-auth/login/`, payload.data);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return danptestdeAPI.get(`/rest-auth/logout/`)
+  return danptestdeAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return danptestdeAPI.post(`/rest-auth/logout/`)
+  return danptestdeAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_registration_create(payload) {
-  return danptestdeAPI.post(`/rest-auth/registration/`, payload.data)
+  return danptestdeAPI.post(`/rest-auth/registration/`, payload.data);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return danptestdeAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return danptestdeAPI.post(`/rest-auth/password/reset/`, payload.data);
 }
+
 function rest_auth_password_change_create(payload) {
-  return danptestdeAPI.post(`/rest-auth/password/change/`, payload.data)
+  return danptestdeAPI.post(`/rest-auth/password/change/`, payload.data);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return danptestdeAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
+  return danptestdeAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return danptestdeAPI.post(
-    `/rest-auth/registration/verify-email/`,
-    payload.data
-  )
+  return danptestdeAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
 }
+
 export const apiService = {
   api_v1_login_create,
   api_v1_signup_create,
@@ -65,4 +82,4 @@ export const apiService = {
   rest_auth_password_change_create,
   rest_auth_password_reset_confirm_create,
   rest_auth_registration_verify_email_create
-}
+};
